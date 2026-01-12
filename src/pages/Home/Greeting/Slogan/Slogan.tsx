5ec06f5f15737198ffd92141128376052c70085b
Alex_B012 <rad15on2014@gmail.com>
2026-01-12 22:00:22 +0300

    productCard img & text - fixed

--- src/pages/Home/Greeting/Slogan/Slogan.tsx
+++ src/pages/Home/Greeting/Slogan/Slogan.tsx
@@ -6,7 +6,7 @@ import { profileData } from '../../../../data/data'
 function Slogan() {
    return (
       <div className="slogan">
-         <h2 className="slogan__title">Lorem ipsum dolor sit.</h2>
+         <h2 className="slogan__title">Лучшее для вашего здоровья</h2>
          <h3 className="slogan__subtitle">Lorem ipsum dolor sit amet.</h3>
          <Link className="slogan__btn" to="/about">
             {`Подробнее о ${profileData.name}`}
