aade3e6716a8ab0aa36b079e53b2b5d249bba03a
Alex_B012 <rad15on2014@gmail.com>
2026-01-16 23:23:34 +0300

    new images - p1 - new format - completed

--- src/data/product_images.ts
+++ src/data/product_images.ts
@@ -1,14 +1,16 @@
 import inotex_img from "../assets/product_images/01_Inotex_img.png";
-import fenolive_img from '../assets/product_images/fenolife_shadow.png';
+// import fenolive_img from '../assets/product_images/fenolife_shadow.png';
+import fenolive_img from '../assets/product_images/fenolife_shadow.webp';
 
 import citikomed_img from '../assets/product_images/03_Citikomed_img.png';
-import androprostPlus_img from '../assets/product_images/androprost_shadow.png';
+// import androprostPlus_img from '../assets/product_images/androprost_shadow.png';
+import androprostPlus_img from '../assets/product_images/androprost_shadow.webp';
 
 import androsan_img from '../assets/product_images/05_Androsan_img.png';
 import viponefron_img from '../assets/product_images/06_Viponefron_img.png';
 
-import aminoprom_img from '../assets/product_images/aminoprim_shadow.png';
-
+// import aminoprom_img from '../assets/product_images/aminoprim_shadow.png';
+import aminoprom_img from '../assets/product_images/aminoprim_shadow.webp';
 
 export const productImages: { [key: string]: string } = {
    inotex_img: inotex_img,
