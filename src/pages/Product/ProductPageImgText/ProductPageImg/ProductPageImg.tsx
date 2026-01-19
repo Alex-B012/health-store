605d68cb64a34fd35de19aecf6ebccf35aaae034
Alex-B012 <rad15on2014@gmail.com>
2026-01-19 23:17:04 +0300

    product images - completed v1.01

--- src/pages/Product/ProductPageImgText/ProductPageImg/ProductPageImg.tsx
+++ src/pages/Product/ProductPageImgText/ProductPageImg/ProductPageImg.tsx
@@ -1,23 +1,35 @@
-import './productPageImg.scss'
+import "./productPageImg.scss";
 
 interface ProductPageImgProps {
-   img: { src: string, rotation?: string },
-   name?: string
+  img: { src: string; rotation?: string };
+  name?: string;
 }
 
 function ProductPageImg({ img, name }: ProductPageImgProps) {
-   let imageRotation = "";
+  let imageRotation = "";
+  let imageLandscapeUp = "";
 
-   if (img.rotation) {
-      imageRotation = img.rotation;
-   }
+  if (img.rotation) {
+    imageRotation = img.rotation;
 
+    if (img.rotation === "landscape") {
+      if (name === "Цитикомед") {
+        imageLandscapeUp = "up";
+      } else if (name === "Андросан") {
+        imageLandscapeUp = "up-androsan";
+      }
+    }
+  }
 
-   return (
-      <div className="productPageImg">
-         <img className={`productPageImg__img productPageImg__img--${imageRotation}`} src={img.src} alt={name} />
-      </div>
-   )
+  return (
+    <div className="productPageImg">
+      <img
+        className={`productPageImg__img ${imageRotation && `productPageImg__img--${imageRotation}`} ${imageLandscapeUp && `productPageImg__img--${imageLandscapeUp}`}`}
+        src={img.src}
+        alt={name}
+      />
+    </div>
+  );
 }
 
-export default ProductPageImg
+export default ProductPageImg;
