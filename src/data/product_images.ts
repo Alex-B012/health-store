27cd738a6658fed46a1564858eee84877aee8aec
Alex-B012 <rad15on2014@gmail.com>
2026-01-19 22:44:03 +0300

    product images - completed

--- src/data/product_images.ts
+++ src/data/product_images.ts
@@ -1,24 +1,17 @@
-import inotex_img from "../assets/product_images/01_Inotex_img.png";
-// import fenolive_img from '../assets/product_images/fenolife_shadow.png';
-import fenolive_img from '../assets/product_images/fenolife_shadow.webp';
-
-import citikomed_img from '../assets/product_images/03_Citikomed_img.png';
-// import androprostPlus_img from '../assets/product_images/androprost_shadow.png';
-import androprostPlus_img from '../assets/product_images/androprost_shadow.webp';
-
-import androsan_img from '../assets/product_images/05_Androsan_img.png';
-import viponefron_img from '../assets/product_images/06_Viponefron_img.png';
-
-// import aminoprom_img from '../assets/product_images/aminoprim_shadow.png';
-import aminoprom_img from '../assets/product_images/aminoprim_shadow.webp';
+import inotex_img from "../assets/product_images/inotex.webp";
+import fenolive_img from "../assets/product_images/fenolife_shadow.webp";
+import citikomed_img from "../assets/product_images/citkomed.webp";
+import androprostPlus_img from "../assets/product_images/androprost_shadow.webp";
+import androsan_img from "../assets/product_images/androsan.webp";
+import viponefron_img from "../assets/product_images/viponefron.webp";
+import aminoprom_img from "../assets/product_images/aminoprim_shadow.webp";
 
 export const productImages: { [key: string]: string } = {
-   inotex_img: inotex_img,
-   fenolive_img: fenolive_img,
-   citikomed_img: citikomed_img,
-   androprostPlus_img: androprostPlus_img,
-   androsan_img: androsan_img,
-   viponefron_img: viponefron_img,
-   aminoprom_img: aminoprom_img,
-}
-
+  inotex_img: inotex_img,
+  fenolive_img: fenolive_img,
+  citikomed_img: citikomed_img,
+  androprostPlus_img: androprostPlus_img,
+  androsan_img: androsan_img,
+  viponefron_img: viponefron_img,
+  aminoprom_img: aminoprom_img,
+};
--- src/pages/Home/ProductCardList/ProductCard/ProductCard.tsx
+++ src/pages/Home/ProductCardList/ProductCard/ProductCard.tsx
@@ -1,33 +1,40 @@
-import { Link } from 'react-router-dom';
-import './productCard.scss';
-import ProductCardBtn from './ProductCardBtn/ProductCardBtn';
+import { Link } from "react-router-dom";
+import "./productCard.scss";
+import ProductCardBtn from "./ProductCardBtn/ProductCardBtn";
 
 interface ProductCardProps {
-   product?: any
+  product?: any;
 }
 
 function ProductCard({ product }: ProductCardProps) {
-   let imageRotationClass = '';
+  let imageRotationClass = "";
+  let imagePortraitLg = "";
 
-   if (product.image && product.image.rotation)
-      imageRotationClass = product.image.rotation;
+  if (product.image && product.image.rotation)
+    imageRotationClass = product.image.rotation;
 
+  if (product.title_data.name === "Цитикомед") imagePortraitLg = "scale";
 
-   return (
-      <div className='product-card'>
-         <Link className='product-card__link' to={`/products/${product?.id}`}>
-            <div className="product-card__img-container">
-               <img className={`product-card__img 
-                  product-card__img--${imageRotationClass}`}
-                  src={product?.image.src}
-                  alt={product?.name} />
-            </div>
-            <h2 className='product-card__title'>{product?.title_data.name}</h2>
-            <p className="product-card__desc">{product?.dosage_form}</p>
-            <ProductCardBtn text={"Подробнее"} />
-         </Link>
-      </div>
-   )
+  if (product.title_data.name === "Андросан")
+    imagePortraitLg = "scale-androsan";
+
+  return (
+    <div className="product-card">
+      <Link className="product-card__link" to={`/products/${product?.id}`}>
+        <div className="product-card__img-container">
+          <img
+            className={`product-card__img 
+                  product-card__img--${imageRotationClass}  product-card__img--${imagePortraitLg}`}
+            src={product?.image.src}
+            alt={product?.name}
+          />
+        </div>
+        <h2 className="product-card__title">{product?.title_data.name}</h2>
+        <p className="product-card__desc">{product?.dosage_form}</p>
+        <ProductCardBtn text={"Подробнее"} />
+      </Link>
+    </div>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
